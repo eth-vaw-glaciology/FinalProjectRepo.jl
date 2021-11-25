b9a2009b9c6c9eff64e689a1e9d081df6e145b32
Ludovic Raess <ludovic.rass@gmail.com>
2021-11-25 23:11:13 +0100

    Update ref tests.

--- test/part1.jl
+++ test/part1.jl
@@ -7,7 +7,7 @@ include("../scripts-part1/part1.jl") # modify to include the correct script
 
 # Reference test using ReferenceTests.jl
 "Compare all dict entries"
-comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-4) for (v1,v2) in zip(values(d1), values(d2))])
+comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (v1,v2) in zip(values(d1), values(d2))])
 inds = Int.(ceil.(LinRange(1, length(Xc_g), 12)))
 d = Dict(:X=> Xc_g[inds], :H_g=>H_g[inds])
 
