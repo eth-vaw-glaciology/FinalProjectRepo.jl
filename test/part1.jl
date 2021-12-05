ea6e75d078bbc11730e571aaacc9ebba27fe0fd0
Ludovic Raess <ludovic.rass@gmail.com>
2021-12-05 10:42:57 +0100

    Update reference tests.
    Now correctly and consistency testing the 3D output `H` array.

--- test/part1.jl
+++ test/part1.jl
@@ -8,8 +8,8 @@ include("../scripts-part1/part1.jl") # modify to include the correct script
 # Reference test using ReferenceTests.jl
 "Compare all dict entries"
 comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (v1,v2) in zip(values(d1), values(d2))])
-inds = Int.(ceil.(LinRange(1, length(Xc_g), 12)))
-d = Dict(:X=> Xc_g[inds], :H_g=>H_g[inds])
+inds = Int.(ceil.(LinRange(1, length(xc_g), 12)))
+d = Dict(:X=> xc_g[inds], :H=>H[inds,inds,inds])
 
 @testset "Ref-file" begin
     @test_reference "reftest-files/test.bson" d by=comp
