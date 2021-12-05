d8b1d6399a83a19e1a96e4597949d3579efd983b
Ludovic Raess <ludovic.rass@gmail.com>
2021-12-05 11:08:29 +0100

    Update reference-tests to run with Julia v1.6.
    Reference tests now work with Julia v1.6 and implement testing a slice of the 3D H array at low resolution `nx=ny=nz=30`.

--- test/part1.jl
+++ test/part1.jl
@@ -9,8 +9,8 @@ include("../scripts-part1/part1.jl") # modify to include the correct script
 "Compare all dict entries"
 comp(d1, d2) = keys(d1) == keys(d2) && all([ isapprox(v1, v2; atol = 1e-5) for (v1,v2) in zip(values(d1), values(d2))])
 inds = Int.(ceil.(LinRange(1, length(xc_g), 12)))
-d = Dict(:X=> xc_g[inds], :H=>H[inds,inds,inds])
+d = Dict(:X=> xc_g[inds], :H=>H[inds, inds, 15])
 
 @testset "Ref-file" begin
-    @test_reference "reftest-files/test.bson" d by=comp
+    @test_reference "reftest-files/test_1.bson" d by=comp
 end
